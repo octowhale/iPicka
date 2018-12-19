60e0eba79619050207598038805f57251c90b0b1
tangxin <tangxin@tangxin-mbp.local>
2018-12-19 21:58:54 +0800

    upload: 增加目录循环

--- command/upload.go
+++ command/upload.go
@@ -1,9 +1,12 @@
 package command
 
 import (
+	"io/ioutil"
+	"os"
 	"path"
 
 	"github.com/octowhale/iPicka/storage"
+	"github.com/octowhale/iPicka/utils"
 )
 
 // ipickaer is the interface
@@ -25,8 +28,46 @@ func upload(filepath string) {
 		ipic = &storage.AliyunOSS{c.Key, c.Sec, c.Endpoint, c.Bucket, c.CustomDomain}
 	}
 
-	// objectKey, filepath := os.Args[1], os.Args[2]
-	objectKey := c.Prefix + path.Base(filepath)
-	ipic.Put(objectKey, filepath)
+	// path exists
+	fi, err := os.Stat(filepath)
+	if os.IsNotExist(err) {
+		utils.Logger().Errorln(err)
+		os.Exit(1)
+	}
 
+	// path is dir
+	if fi.IsDir() {
+		files, _, _ := fileAndDir(filepath)
+		for _, subFilepath := range files {
+			objectKey := c.Prefix + path.Base(subFilepath)
+			ipic.Put(objectKey, subFilepath)
+		}
+	} else {
+		// objectKey, filepath := os.Args[1], os.Args[2]
+		objectKey := c.Prefix + path.Base(filepath)
+		ipic.Put(objectKey, filepath)
+	}
+
+}
+
+func fileAndDir(dirname string) (files []string, dirs []string, err error) {
+
+	dir, err := ioutil.ReadDir(dirname)
+	// fmt.Println(dir)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	pathSep := string(os.PathSeparator)
+
+	for _, fi := range dir {
+		// fmt.Println(pos)
+		if fi.IsDir() {
+			dirs = append(dirs, dirname+pathSep+fi.Name())
+		} else {
+			files = append(files, dirname+pathSep+fi.Name())
+		}
+	}
+	// fmt.Println(files)
+	return
 }
